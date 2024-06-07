57ea38dc03c2ceb72b2b3035ce2c97fdd5627991
sarafashka <sarafanova.nv@gmail.com>
2024-06-07 10:06:44 +0300

    feat: add base styles

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,11 @@
 const App = () => {
   return (
     <>
-      <div>
-        Init
-      </div>
+      <h1>
+        text h1
+      </h1>
+      <h2>text h2</h2>
+      <h3>text h3</h3>
     </>
   )
 }
