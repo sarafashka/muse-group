e128d537c1f539813d6bee2b63ece297d6f80947
sarafashka <sarafanova.nv@gmail.com>
2024-06-07 19:29:00 +0300

    feat: add footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,36 @@
+import styles from './Footer.module.scss';
+
+const footerLinks = [
+  {name: 'Help Center',
+   url: 'https://support.animaapp.com/en/'
+  },
+  {name: 'Privacy Policy',
+  url: 'https://forum.animaapp.com/privacy'
+ },
+  {name: 'Terms of Service',
+  url: 'https://forum.animaapp.com/tos'
+  },
+]
+
+const {footer, footer__link} = styles;
+
 const Footer:React.FC = () => {
+
   return(
     <>
-    <div>footer</div>
+    <footer className="container">
+      <div className={footer}> 
+        {footerLinks.map((item, index) => (
+          <a className={footer__link}
+          key={index}
+          href={item.url}
+          target="_blank"
+          rel="noopener noreferrer">
+            {item.name}
+        </a>
+       ))}
+      </div>
+    </footer>
     </>
   )
 };
--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,7 +1,26 @@
+import styles from './ProductCard.module.scss'
+
 const ProductCard = () => {
+  const {
+    product,
+    product__about,
+    product__name,
+    product__image,
+    product__description,
+    product__details
+  } = styles;
   return (
     <>
-    <div> block of products</div>
+    <div className={product}>
+      <div className={product__image}></div>
+      <div className={product__about}>
+        <h4 className={product__name}>Ultimate Guitar</h4>
+        <div className={ product__description}>The best platform for music makers to play songs they love. Home to the world's largest music community.</div>
+        <button className={product__details}>Read more</button>
+      </div>
+     
+    </div>
+
     </>
   )
 }
