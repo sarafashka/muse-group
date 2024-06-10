c8df47d37f1344de5e13eed9658f4c9f921ba813
sarafashka <sarafanova.nv@gmail.com>
2024-06-10 20:10:15 +0300

    test vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  let baseConfig = '';
-  if (command === 'build') {
-    baseConfig = '/muse-group';
-  }
-  if (command === 'serve') {
-    baseConfig = '/';
-  }
-  const config = {
-    plugins: [react()],
-    base: baseConfig,
-  };
-
-  return config;
+export default defineConfig({
+  base: '/muse-group/', // Replace 'my-react-app' with your repository name
+  plugins: [react()],
 });
